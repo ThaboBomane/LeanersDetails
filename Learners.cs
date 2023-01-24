5fd9bbee9ab72d3e0e5c657331a2952e26a82d1a
ThaboBomane <thabobomane2@gmail.com>
2023-01-24 12:55:45 +0200

    Add project files.

---
+++ Learners.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnersDetails
+{
+     class Learners
+    {
+        public string firstname;
+        public string lastname;
+        public string gender;
+        public int age;
+        public char initials;
+        public Learners (String Firstname, string Lastname, string Gender, int Age, char Initials)
+        {
+            firstname = Firstname;
+            lastname = Lastname;
+            gender = Gender;
+            age = Age;
+            initials = Initials;
+
+        }
+    }
+    
+   
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using LearnersDetails;
+class Program
+{
+    static void Main (string[] args)
+    {
+        Learners learner1 = new Learners("Thabo", "Tauyatsoala", "Female", 34, 'T');
+
+        Console.WriteLine(learner1.firstname + " " + learner1.lastname + " " + learner1.gender + " " + learner1.age + " " + learner1.initials);
+    }
+}
